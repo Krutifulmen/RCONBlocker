cfadc629db15f00c4b46e1dad8b80b3c57167aab
Krutifulmen <143906543+Krutifulmen@users.noreply.github.com>
2026-02-05 17:00:36 +0300

    Create RconBlockerPlugin.java

---
+++ src/main/java/xyz/imperiumsmp/rcon/RconBlockerPlugin.java
@@ -0,0 +1,29 @@
+package xyz.imperiumsmp.rcon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class RconBlockerPlugin extends JavaPlugin {
+
+    private static RconBlockerPlugin instance;
+    private List<String> allowedIps;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        allowedIps = getConfig().getStringList("allowed-ips");
+
+        getLogger().info("Injecting into RCON...");
+        RconInjector.inject();
+    }
+
+    public static RconBlockerPlugin getInstance() {
+        return instance;
+    }
+
+    public List<String> getAllowedIps() {
+        return allowedIps;
+    }
+}
